f81169d22ccb1bd2661f2c814b8ce22ed0876964
André Pereira <andre.pereira@mov.ai>
2024-04-11 11:31:10 +0100

    [SKIP] Automatic version bump 2.5.0-7 -> 2.6.0-0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setuptools.setup(
     name="gd-node",
-    version="2.5.0-7",
+    version="2.6.0-0",
     author="Backend team",
     author_email="backend@mov.ai",
     description="GD_Node",
