d36445c48fb4c7f52cbcc44f01032435abe17b5d
OttoMation-Movai <automation@mov.ai>
2024-05-23 18:37:20 +0000

    [skip actions] Automatic Bump of build version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setuptools.setup(
     name="gd-node",
-    version="2.5.0-15",
+    version="2.5.0-16",
     author="Backend team",
     author_email="backend@mov.ai",
     description="GD_Node",
